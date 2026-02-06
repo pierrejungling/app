2221bd5ed1f3c359670cf9790702d211b4370e65
Pierre Jungling <pierrejungling@gmail.com>
2026-02-06 01:44:49 +0100

    dom test 2

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://api.gravisterie.be',
+  apiURL: 'https://api.gravisterie.be/',
   PARAM_IS_MISSING: 499,
   TOKEN_KEY: 'token'
 };
